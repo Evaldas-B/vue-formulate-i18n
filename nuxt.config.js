58ccd950bd174fb778a4a98a3ceca267e04f650f
Evaldas Bendoraitis <bendoraitis.evaldas@gmail.com>
2021-02-09 13:35:11 +0200

    Add nuxt/i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,23 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'vue-formulate-i18n',
+    title: "vue-formulate-i18n",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -29,14 +25,30 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ["nuxt-i18n"],
+
+  i18n: {
+    locales: [
+      {
+        name: "English",
+        code: "en",
+        file: "en-US.json"
+      },
+      {
+        name: "Lietuvių",
+        code: "lt",
+        file: "lt-LT.json"
+      }
+    ],
+    defaultLocale: "lt",
+    lazy: true,
+    langDir: "locales/"
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center mx-auto text-center mt-56">
+    <h1 class="text-6xl text-gray-700">{{ $t("hello") }},</h1>
+
+    <span class="mt-5 text-lg">Languages: </span>
+
+    <nuxt-link :to="switchLocalePath('en')">
+      <button
+        class="my-5 py-1 text-white px-3 bg-green-600 hover:bg-green-700 rounded"
+      >
+        English
+      </button>
+    </nuxt-link>
+
+    <nuxt-link :to="switchLocalePath('lt')">
+      <button
+        class="py-1 text-white px-3 bg-green-600 hover:bg-green-700 rounded"
+      >
+        Lietuvių
+      </button>
+    </nuxt-link>
+  </div>
 </template>
